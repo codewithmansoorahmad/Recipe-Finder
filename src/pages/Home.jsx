fb36c9c73e07f32bb0eaab37cba09c1c54cb6ad4
Mansoor Ahmad <mansoorkha9890@gmail.com>
2026-09-12 16:32:40 +0500

    call api for recipes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react"
 import Hero from "../components/Hero"
 
 import "../css/Home.css"
+import { callApi } from "../services/api"
 export default function Home(){
+    const [recipes,setRecipes]=useState([])
+    useEffect(()=>{
+       async function getRecipes(){
+let data=await callApi(6)
+setRecipes(data.recipes)
+        }
+        getRecipes()
+
+    },[])
+    console.log(recipes)
     return <div className="home">
         <Hero/>
 
